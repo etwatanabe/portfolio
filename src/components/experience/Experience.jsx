1bbc088a1e0ca4ed2b5117c1a1c61556dc994de7
Eduardo Takeshi <wattksh@gmail.com>
2025-05-11 17:11:52 -0300

    update experience section

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -1,14 +1,9 @@
-import React, { useState } from "react";
+import React from "react";
 import "./experience.css";
 import ExperienceDataLeft from "./LeftExperienceData";
 import ExperienceDataRight from "./ExperienceDataRight";
 
 const Experience = () => {
-  const [toggleState, setToggleState] = useState(1);
-
-  const toggleTab = (index) => {
-    setToggleState(index);
-  };
 
   return (
     <section className="experience section" id="experience">
@@ -16,30 +11,12 @@ const Experience = () => {
       <span className="section__subtitle">My professional journey</span>
 
       <div className="experience__container container">
-        <div className="experience__tabs">
-          <div className={toggleState === 1 ? "experience__button experience__active button--flex" : "experience__button button--flex"}  onClick={()=>toggleTab(1)}>
-            <i className="uil uil-briefcase-alt experience__icon"></i>{" "}
-            Experience
-          </div>
-
-          <div className={toggleState === 2 ? "experience__button experience__active button--flex" : "experience__button button--flex"} onClick={()=>toggleTab(2)}>
-            <i className="uil uil-graduation-cap experience__icon"></i>{" "}
-            Education
-          </div>
-        </div>
-
-        <div className="experience__sections">
-          <div className="experience__content-active">
+          <div className="experience__content">
             <ExperienceDataLeft title="Mobile Developer" subtitle="CEPEDI" calendar="07/2024 - Present" />
             <ExperienceDataRight title="Software Developer" subtitle="CEPEDI" calendar="10/2022 - 05/2023" />
             <ExperienceDataLeft title="Research, Development & Innovation Manager" subtitle="Life Jr." calendar="03/2019 - 03/2020" />
           </div>
-          <div className={toggleState === 2 ? "experience__content experience__content-active" : "experience__content"}>
-            <ExperienceDataRight title="Computer Science" subtitle="Universidade Estadual de Santa Cruz" calendar="03/2022 - Present" />
-          </div>
-
         </div>
-      </div>
     </section>
   );
 };
