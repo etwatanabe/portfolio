83824b0206126a55ecb6c2ca8f79749109747b58
Eduardo Takeshi <wattksh@gmail.com>
2025-05-11 22:09:13 -0300

    revert vite.config

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,5 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  resolve: {
-    extensions: [".jsx", ".js", ".ts", ".tsx"],
-  },
   plugins: [react()],
 });
