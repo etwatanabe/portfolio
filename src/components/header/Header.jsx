9f2ed7058f060375ec68a30e2039fd81e0252b98
Eduardo Takeshi <wattksh@gmail.com>
2025-05-11 23:56:17 -0300

    fix header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -18,7 +18,7 @@ const Header = () => {
   return (
     <header className="header">
       <nav className="nav container">
-        <a href="/" className="nav__logo">ETW</a>
+        <a href="/portfolio" className="nav__logo">ETW</a>
 
         <div className={Toggle ? "nav__menu show-menu" : "nav__menu"}>
           <ul className="nav__list grid">
