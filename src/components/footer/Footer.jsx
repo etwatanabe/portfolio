183fa43a965afcf09423297474cf1b94c02a7ede
Eduardo Takeshi <wattksh@gmail.com>
2025-05-11 22:53:06 -0300

    fix footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -8,13 +8,6 @@ const Footer = () => {
         <h1 className="footer__title">ETW</h1>
         
         <div className="footer__social">
-          <a href="https://wa.me/+5573999713191" className="footer__social-link" target="_blank">
-            <i className="bx bxl-whatsapp"></i>
-          </a>
-
-          <a href="mailto:eduardotwatanabe@gmail.com" className="footer__social-link" target="_blank">
-            <i className="bx bx-envelope"></i>
-          </a>
 
           <a href="https://www.linkedin.com/in/etwatanabe" className="footer__social-link" target="_blank">
             <i className="bx bxl-linkedin"></i>
@@ -22,6 +15,14 @@ const Footer = () => {
 
           <a href="https://www.github.com/etwatanabe" className="footer__social-link" target="_blank">
             <i className="bx bxl-github"></i>
+          </a>
+          
+          <a href="https://wa.me/+5573999713191" className="footer__social-link" target="_blank">
+            <i className="bx bxl-whatsapp"></i>
+          </a>
+
+          <a href="mailto:eduardotwatanabe@gmail.com" className="footer__social-link" target="_blank">
+            <i className="bx bx-envelope"></i>
           </a>
         </div>
 
