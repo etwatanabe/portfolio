15e3743839a6d576727492933cea97684463d7e4
Eduardo Takeshi <wattksh@gmail.com>
2025-05-12 21:36:11 -0300

    update skills icons based on theme

--- src/components/skills/SkillIcon.jsx
+++ src/components/skills/SkillIcon.jsx
@@ -1,9 +1,12 @@
-import React from 'react';
+import React, { useContext } from "react";
+import { ThemeContext } from "../../context/ThemeContext";
 
 const SkillIcon = ({ name, icon }) => {
+  const { isDarkTheme } = useContext(ThemeContext);
+
   return (
     <div className="skills__icon-box">
-      <i className={`${icon} colored`}></i>
+      <i className={`${icon} ${!isDarkTheme ? "colored" : ""}`}></i>
       <span>{name}</span>
     </div>
   );
